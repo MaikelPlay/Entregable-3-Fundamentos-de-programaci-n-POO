78cbde79e50362b8cc0078775932a4d1c1d1f4aa
Maikel <maikelcorreoelectronico@gmail.com>
2025-09-16 21:45:37 +0200

    Clase Disco Duro

---
+++ src/DiscoDuro.ts
@@ -0,0 +1,34 @@
+import { IDatos } from "./IDatos.js";
+import { Disco } from "./disco.js";
+
+export class DiscoDuro extends Disco implements IDatos {
+    constructor(nombre: string, capacidad: number, tipo: string, velocidad: number) {
+        super(nombre, capacidad, tipo, velocidad);
+    }  
+
+    escribirDatos(datos: string): void{
+        console.log("Escribiendo datos en el disco duro con un cabezal magnético");
+    }
+    leerDatos(): void {
+        console.log("Leyendo datos del disco duro con un cabezal magnético");
+    }
+    girarDisco(): void {
+        console.log(`El CD ${this.nombre} está girando a ${this.velocidad}.`);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
