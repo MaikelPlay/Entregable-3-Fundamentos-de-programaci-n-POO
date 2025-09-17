eb4d83bd01bcf993ac0eab590bea7dbd11269a5d
Maikel <maikelcorreoelectronico@gmail.com>
2025-09-17 14:16:42 +0200

    Ejercicio Finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import { Cd } from "./Cd.js";
 import { DiscoDuro } from "./DiscoDuro.js";
+import { Vinilo } from "./Vinilo.js";
+import { Rueda } from "./Rueda.js";
+import { Fresbee } from "./Fresbee.js";
+import { Blueray } from "./Blueray.js";
+
 
 
 const CD1 = new Cd("CD1", 700, "Música", "CD-ROM", 200);
 const CD2 = new Cd("CD2", 600, "Fotos", "CD-RW", 300);
 const DiscoDuro1 = new DiscoDuro("DiscoDuro1", 1000000, "Documentos", "HDD", 7200);
 const DiscoDuro2 = new DiscoDuro("DiscoDuro2", 2000000, "Videos", "SSD", 10000);
-const Vinilo1 = new DiscoDuro("Vinilo1", 500, "Música clásica", "Vinilo", 33);
-const Vinilo2 = new DiscoDuro("Vinilo2", 1000, "Rock", "Vinilo", 45);
-const Blueray1 = new DiscoDuro("Blu-ray1", 25000, "Películas", "Blu-ray", 6000);
-const Blueray2 = new DiscoDuro("Blu-ray2", 50000, "Juegos", "Blu-ray", 12000);
-const Rueda1 = new DiscoDuro("Rueda1", 0, "N/A", "Rueda", 0);
-const Rueda2 = new DiscoDuro("Rueda2", 0, "N/A", "Rueda", 0);
-const Fresbee1 = new DiscoDuro("Fresbee1", 0, "N/A", "Fresbee", 0);
-const Fresbee2 = new DiscoDuro("Fresbee2", 0, "N/A", "Fresbee", 0);
+const Vinilo1 = new Vinilo("Vinilo1", 500, "Música clásica", "Vinilo", 33);
+const Vinilo2 = new Vinilo("Vinilo2", 1000, "Rock", "Vinilo", 45);
+const Blueray1 = new Blueray("Blu-ray1", 25000, "Películas", "Blu-ray", 6000);
+const Blueray2 = new Blueray("Blu-ray2", 50000, "Juegos", "Blu-ray", 12000);
+const Rueda1 = new Rueda("Rueda1", 0, "N/A", "Rueda", 0);
+const Rueda2 = new Rueda("Rueda2", 0, "N/A", "Rueda", 0);
+const Fresbee1 = new Fresbee("Fresbee1", 0, "N/A", "Fresbee", 0);
+const Fresbee2 = new Fresbee("Fresbee2", 0, "N/A", "Fresbee", 0);
 
 
 
